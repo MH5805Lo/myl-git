6a9a0d8cc2d73e54f80cbd14f2d00d364521c5c0
yonglong.ma <yonglong.malong1112>
2020-09-28 10:15:21 +0800

    测试提交github

---
+++ src/test/java/com/spring5/myl/SpringbootParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.myl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
