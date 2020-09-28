bbf3332c37ff6051b03f606af09774799339a95e
yonglong.ma <yonglong.malong1112>
2020-09-28 22:09:37 +0800

    springboot

--- src/main/java/com/spring5/myl/SpringbootParentApplication.java
+++ src/main/java/com/spring5/myl/SpringbootParentApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootParentApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootParentApplication.class, args);
     }
