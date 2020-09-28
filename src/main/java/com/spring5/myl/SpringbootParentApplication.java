672201acb38070201befea29a5b90370f163576e
yonglong.ma <yonglong.malong1112>
2020-09-28 09:56:01 +0800

    测试提交github

---
+++ src/main/java/com/spring5/myl/SpringbootParentApplication.java
@@ -0,0 +1,13 @@
+package com.spring5.myl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootParentApplication.class, args);
+    }
+
+}
